f0919be1585c5a2f87ec8b64e08e83a89054d3c7
Seamus Mullan <24carrotgaming@gmail.com>
2022-09-25 00:53:58 +0100

    Need to fix spawning, something about modevenbus and forge event bus

--- src/main/java/com/seamusmullan/arrowgolems/ArrowGolems.java
+++ src/main/java/com/seamusmullan/arrowgolems/ArrowGolems.java
@@ -1,7 +1,9 @@
 package com.seamusmullan.arrowgolems;
 
 import com.mojang.logging.LogUtils;
+import com.seamusmullan.arrowgolems.entity.ModEntities;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
@@ -35,10 +37,13 @@ public class ArrowGolems
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
+    // add entities registry
+
+
+
+
     public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
     // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
     public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
@@ -54,7 +59,7 @@ public class ArrowGolems
         BLOCKS.register(modEventBus);
         // Register the Deferred Register to the mod event bus so items get registered
         ITEMS.register(modEventBus);
-
+        ModEntities.register(modEventBus);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
